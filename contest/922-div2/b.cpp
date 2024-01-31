a358c3fe0f64718734c90437c897e1aeffd7c1dc
kishan <kishanknows@gmail.com>
2024-01-31 11:38:42 +0530

    Codeforces Round 922 (Div. 2)

---
+++ contest/922-div2/b.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#define int long long
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<pair<int, int>> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i].first;
+    for (int i = 0; i < n; i++) cin >> a[i].second;
+    sort(a.begin(), a.end());
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+        cout << a[i].first << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < n; i++) {
+        cout << a[i].second << " ";
+    }
+    cout << endl;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
