d33c93c1d37f6f348c8a236259c65402a10fd740
kishan <kishanknows@gmail.com>
2024-06-02 13:33:12 +0530

    C. Lexicographically Largest

---
+++ practice/1700/1930C.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#define int long long
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+    tree_order_statistics_node_update> ordered_set;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        a[i] += i;
+    }
+    a[0] = 1e10;
+    sort(a.rbegin(), a.rend());
+    for (int i = 1; i <= n; i++) {
+        a[i] = min(a[i], a[i - 1] - 1);
+    }
+    for (int i = 1; i <= n; i++) cout << a[i] << " ";
+    cout << endl;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    // int t = 1; 
+    int t; cin >> t;
+    while (t--) solve();
+    return 0;
+}
